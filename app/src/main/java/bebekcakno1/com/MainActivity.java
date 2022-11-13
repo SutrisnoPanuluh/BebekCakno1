7ea9b6e9f45600dc851ae543be2d33ea7c926818
LAPTOP-JF5T6NS4\Sutrisno <Sutresfendik@gmail.com>
2022-11-13 14:35:01 +0700

    Initial commit

---
+++ app/src/main/java/bebekcakno1/com/MainActivity.java
@@ -0,0 +1,28 @@
+package bebekcakno1.com;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/bebekcakno1/com/MainActivity3.java
@@ -0,0 +1,14 @@
+package bebekcakno1.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
